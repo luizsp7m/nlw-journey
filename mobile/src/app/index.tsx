270bc15210ec88c26bad1c2717aaac7bce851ed2
Luiz Oliveira <luizoliveira2808@gmail.com>
2024-07-08 23:15:29 -0300

    aula 01 pt.1

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,75 @@
+import { Input } from "@/components/input";
+import { Image, Text, View } from "react-native";
+import { MapPin, Calendar as CalendarIcon, Settings2, UserRoundPlus, ArrowRight } from 'lucide-react-native'
+import { colors } from "@/styles/colors";
+import { Button } from "@/components/button";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center px-5">
+      <Image
+        source={require('@/assets/logo.png')}
+        className="h-8"
+        resizeMode="contain"
+      />
+
+      <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
+        Convide seus amigos e planeje sua {"\n"} próxima viagem
+      </Text>
+
+      <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
+        <Input variant="primary">
+          <MapPin
+            color={colors.zinc[400]}
+            size={20}
+          />
+
+          <Input.Field placeholder="Para onde?" />
+        </Input>
+
+        <Input>
+          <CalendarIcon
+            color={colors.zinc[400]}
+            size={20}
+          />
+
+          <Input.Field placeholder="Quando?" />
+        </Input>
+
+        <View className="border-b py-3 border-zinc-800">
+          <Button variant="secondary">
+            <Button.Title>Alterar local/data</Button.Title>
+
+            <Settings2
+              color={colors.zinc[200]}
+              size={20}
+            />
+          </Button>
+        </View>
+
+        <Input>
+          <UserRoundPlus
+            color={colors.zinc[400]}
+            size={20}
+          />
+
+          <Input.Field placeholder="Quem estará na viagem?" />
+        </Input>
+
+        <Button>
+          <Button.Title>Continuar</Button.Title>
+
+          <ArrowRight
+            color={colors.lime[950]}
+            size={20}
+          />
+        </Button>
+      </View>
+
+      <Text className="text-zinc-500 text-center font-regular text-base">
+        Ao planejar sua viagem pela plann.er você automaticamente concorda com nossos {" "}
+        <Text className="text-zinc-300 underline">termos de uso e políticas de privacidade</Text>
+      </Text>
+    </View>
+  );
+}
---
+++ mobile/src/components/button.tsx
@@ -0,0 +1,72 @@
+import clsx from 'clsx'
+
+import {
+  Text,
+  TextProps,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  ActivityIndicator,
+} from "react-native";
+
+import { createContext, useContext } from "react";
+
+type Variants = 'primary' | 'secondary'
+
+type ButtonProps = TouchableOpacityProps & {
+  variant?: Variants
+  isLoading?: boolean
+}
+
+const ThemeContext = createContext({} as { variant: Variants });
+
+function Button({
+  variant = 'primary',
+  isLoading = false,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <ThemeContext.Provider value={{
+      variant
+    }}>
+      <TouchableOpacity
+        disabled={isLoading}
+        activeOpacity={0.70}
+        {...props}
+      >
+        <View
+          className={clsx(
+            "w-full h-11 flex-row items-center justify-center rounded-lg gap-2",
+            {
+              "bg-lime-300": variant === 'primary',
+              "bg-zinc-800": variant === 'secondary'
+            },
+          )}
+        >
+          {isLoading ? <ActivityIndicator className='text-lime-950' /> : children}
+        </View>
+      </TouchableOpacity>
+    </ThemeContext.Provider>
+  )
+}
+
+function Title({ children }: TextProps) {
+  const { variant } = useContext(ThemeContext);
+
+  return (
+    <Text
+      className={clsx(
+        "",
+        { "text-lime-950": variant === 'primary' },
+        { "text-zinc-200": variant === 'secondary' },
+      )}
+    >
+      {children}
+    </Text>
+  )
+}
+
+Button.Title = Title
+
+export { Button } 
